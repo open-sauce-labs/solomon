996e09292b73fa55d8754441bf6d7bfedf3ee0c6
josip-volarevic <josip.volarevic4@gmail.com>
2022-09-07 17:34:05 +0200

    chore: update shortenString utility function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,9 +2,14 @@ export function sleep(ms: number): Promise<void> {
 	return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-export const shortenString = (string: string, chars = 4): string => {
-	if (string.length < chars * 2 + 3) return string
-	return `${string.slice(0, chars)}...${string.slice(-chars)}`
+/**
+ * @param string string to shorten
+ * @param slice size of a slice
+ * @returns short version of the string in a format '[slice]...[slice]'
+ */
+export const shortenString = (string: string, slice = 4): string => {
+	if (string.length < slice * 2 + 3) return string
+	return `${string.slice(0, slice)}...${string.slice(-slice)}`
 }
 
 export const getUnixTimeInSeconds = () => {
